e70a44c2d0ccc0a3cdd0901b22af00ac6711981b
Ramadhan <makbarramadhan62@gmail.com>
2024-06-07 16:43:07 +0700

    style: adjust color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
+  plugins: [daisyui],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +17,21 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#2278D4",
+          secondary_1: "#FF9F66",
+          secondary_2: "#FF5F00",
+          green: "#03A7A0",
+          white_1: "#fff",
+          white_2: "#F2F0F4",
+          black_1: "#191919",
+          black_2: "#343434",
+        },
+      },
+    ],
+  },
 };
 export default config;
