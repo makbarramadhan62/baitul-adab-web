cb7f90322e34fd9fd7ed3d2f0c88d3c143a60880
Ramadhan <makbarramadhan62@gmail.com>
2024-06-07 21:54:16 +0700

    refactor: implement for new component curiculum & testimonial

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-import Navbar from "@/components/navbar";
+import Navbar from "../components/Navbar";
 import Image from "next/image";
+import CurriculumCard from "@/components/CuriculumCard";
+import TestimonialCard from "@/components/TestimonialCard";
+import "swiper/css";
+import { Swiper } from "swiper/react";
+import { SwiperSlide } from "swiper/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <div className="flex min-h-screen w-full flex-col items-center overflow-x-hidden">
       <Navbar />
 
       {/* Hero Section*/}
       <section
         id="hero"
-        className="p-16 flex flex-col lg:flex-row items-center justify-center mt-4"
+        className="p-16 flex flex-col lg:flex-row items-center justify-center mt-4 relative"
       >
         {/* Bagian Kiri */}
         <div className="w-full lg:w-1/2 lg:order-1 mb-8 md:mb-0">
@@ -38,23 +43,159 @@ export default function Home() {
             width={100}
             unoptimized
             style={{ width: "80%", height: "auto" }}
-            src="/image/heroSecrtion-children.png"
+            src="/image/home/heroSecrtion-children.png"
             alt="school-pict"
             className="object-contain mx-auto"
           />
         </div>
+        <Image
+          height={100}
+          width={100}
+          unoptimized
+          style={{ width: "100%", height: "auto" }}
+          src="/image/home/UnionBg.svg"
+          alt="school-pict"
+          className="object-cover absolute bottom-0 lg:-bottom-36"
+        />
       </section>
       {/* Hero Section*/}
 
       {/* Second Section*/}
       <section
         id="second"
-        className="px-16 flex flex-col lg:flex-row items-center justify-center mt-4 w-1/2 lg:w-full lg:order-1 lg:justify-center"
-        style={{ backgroundColor: "#FF9F66" }}
+        className="w-full relative z-10 my-8 bg-primary-content"
       >
-        <span className="text-5xl">Kenapa Harus Baitul Adab?</span>
+        <div className="px-16">
+          <div className="w-full text-center">
+            <span className="text-5xl font-semibold">
+              Kenapa Harus Baitul Adab?
+            </span>
+          </div>
+          <div className="flex flex-col gap-10 items-center justify-center p-8 w-1/2 lg:w-full lg:order-1 lg:justify-center mx-auto">
+            <div className="flex flex-col lg:flex-row items-center justify-center">
+              <CurriculumCard
+                imageSrc="/image/home/BookOpen.svg"
+                imageAlt="school-pict"
+                title="Kurikulum Terbaru"
+                description="Menggunakan pembelajaran dengan kurikulum terupdate"
+              />
+              <CurriculumCard
+                imageSrc="/image/home/SquaresFour.svg"
+                imageAlt="school-pict"
+                title="Fasilitas Lengkap"
+                description="Tersedia berbagai fasilitas untuk menunjang pembelajaran"
+              />
+              <CurriculumCard
+                imageSrc="/image/home/BookOpenUser.svg"
+                imageAlt="school-pict"
+                title="Mengaji Setiap Hari"
+                description="Mengaji setiap hari agar anak fasih membaca Al-Quran"
+              />
+              <CurriculumCard
+                imageSrc="/image/home/Books.svg"
+                imageAlt="school-pict"
+                title="Hafalan Juz 30"
+                description="Anak menghafal Al-Quran surah - surah pendek (Juz 30)"
+              />
+            </div>
+
+            <div className="flex flex-col lg:flex-row items-center justify-center">
+              <CurriculumCard
+                imageSrc="/image/home/BookOpen.svg"
+                imageAlt="school-pict"
+                title="Bilingual Program"
+                description="Pembelajaran menggunakan 2 bahasa (Indonesia - Inggris)"
+              />
+              <CurriculumCard
+                imageSrc="/image/home/Smiley.svg"
+                imageAlt="school-pict"
+                title="Pembiasaan Adab"
+                description="Mempelajari adab sopan santun agar anak berakhlak mulia"
+              />
+              <CurriculumCard
+                imageSrc="/image/home/HandsPraying.svg"
+                imageAlt="school-pict"
+                title="Doa & Ibadah Harian"
+                description="Mempelajari adab sopan santun agar anak berakhlak mulia"
+              />
+            </div>
+          </div>
+        </div>
       </section>
       {/* Second Section*/}
-    </main>
+
+      {/* Third Section*/}
+      <section id="third" className="w-full relative z-10 my-8">
+        <div className="px-16">
+          <div className="w-full text-center mb-8">
+            <div className="flex flex-col justify-center gap-4 text-5xl font-semibold">
+              <span className="text-neutral">
+                Apa Kata Para Orangtua Tentang
+              </span>
+              <span className="text-secondary-content">
+                Baitul Adab Islamic School?
+              </span>
+            </div>
+          </div>
+
+          <div className="carousel gap-8 rounded-box">
+            <div className="carousel-item">
+              <TestimonialCard
+                imageSrc="/image/home/TestimonialPerson.png"
+                imageAlt="school-pict"
+                title="Kurikulum Terbaru"
+                description="Loerem ipsumLoerem ipsumLoerem ipsumLoerem ipsumLoerem ipsum Loerem ipsumLoerem ipsumLoerem ipsum"
+              />
+            </div>
+            <div className="carousel-item">
+              <TestimonialCard
+                imageSrc="/image/home/TestimonialPerson.png"
+                imageAlt="school-pict"
+                title="Kurikulum Terbaru"
+                description="Loerem ipsumLoerem ipsumLoerem ipsumLoerem ipsumLoerem ipsum Loerem ipsumLoerem ipsumLoerem ipsum"
+              />
+            </div>
+            <div className="carousel-item">
+              <TestimonialCard
+                imageSrc="/image/home/TestimonialPerson.png"
+                imageAlt="school-pict"
+                title="Kurikulum Terbaru"
+                description="Loerem ipsumLoerem ipsumLoerem ipsumLoerem ipsumLoerem ipsum Loerem ipsumLoerem ipsumLoerem ipsum"
+              />
+            </div>
+            <div className="carousel-item">
+              <TestimonialCard
+                imageSrc="/image/home/TestimonialPerson.png"
+                imageAlt="school-pict"
+                title="Kurikulum Terbaru"
+                description="Loerem ipsumLoerem ipsumLoerem ipsumLoerem ipsumLoerem ipsum Loerem ipsumLoerem ipsumLoerem ipsum"
+              />
+            </div>
+            <div className="carousel-item">
+              <TestimonialCard
+                imageSrc="/image/home/TestimonialPerson.png"
+                imageAlt="school-pict"
+                title="Kurikulum Terbaru"
+                description="Loerem ipsumLoerem ipsumLoerem ipsumLoerem ipsumLoerem ipsum Loerem ipsumLoerem ipsumLoerem ipsum"
+              />
+            </div>
+          </div>
+
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            ...
+          </Swiper>
+        </div>
+      </section>
+      {/* Third Section*/}
+    </div>
   );
 }
