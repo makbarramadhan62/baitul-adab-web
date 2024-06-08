a15696b5869473e705b32bd80d9c219d4bba7525
Ramadhan <makbarramadhan62@gmail.com>
2024-06-08 21:36:08 +0700

    feat: add footer component

---
+++ components/footer/index.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className="w-full">
+      <Image
+        height={100}
+        width={100}
+        unoptimized
+        style={{ width: "100%", height: "auto" }}
+        src="/image/footer/topFooterBG.svg"
+        alt="school-pict"
+        className="object-contain mx-auto"
+      />
+      <footer className="footer p-10 lg:px-80 gap-24 bg-primary-content text-base-content">
+        <aside className="w-2/3">
+          <div className="flex items-center gap-2">
+            <Image
+              width={100}
+              height={100}
+              style={{ width: "auto", height: "auto" }}
+              src={"/image/whiteLogo.svg"}
+              alt="schools"
+            />
+            <span className="font-bold text-xl text-white">
+              Baitul Adab Islamic School
+            </span>
+          </div>
+          <p className="font-normal text-sm text-white">
+            Lörem ipsum od ohet dilogi. Bell trabel, samuligt, ohöbel utom
+            diska. Jinesade bel när feras redorade i belogi. FAR paratyp i
+            muvåning, och pesask vyfisat. Viktiga poddradio har un mad och inde.
+          </p>
+          <div className="flex items-center gap-2">
+            <button className="btn bg-transparent shadow-none hover:bg-transparent text-white hover:text-neutral text-xl border-0">
+              <i className="bi bi-facebook"></i>
+            </button>
+            <button className="btn bg-transparent shadow-none hover:bg-transparent text-white hover:text-neutral text-xl border-0">
+              <i className="bi bi-twitter"></i>
+            </button>
+            <button className="btn bg-transparent shadow-none hover:bg-transparent text-white hover:text-neutral text-xl border-0">
+              <i className="bi bi-linkedin"></i>
+            </button>
+            <button className="btn bg-transparent shadow-none hover:bg-transparent text-white hover:text-neutral text-xl border-0">
+              <i className="bi bi-instagram"></i>
+            </button>
+          </div>
+        </aside>
+        <nav className="text-white">
+          <h6 className="text-xl font-semibold">Pages</h6>
+          <a className="link link-hover">Home</a>
+          <a className="link link-hover">Profile</a>
+          <a className="link link-hover">Program Sekolah</a>
+          <a className="link link-hover">Gallery Sekolah</a>
+        </nav>
+        <nav className="text-white">
+          <h6 className="text-xl font-semibold">Contact</h6>
+          <div className="flex items-center gap-2">
+            <i className="bi bi-telephone-fill"></i>
+            <span>0856-1623-478</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <i className="bi bi-envelope-fill"></i>
+            <span>baituladabislamicschool@gmail.com</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <i className="bi bi-geo-alt-fill"></i>
+            <span>2972 Westheimer Rd. Santa Ana, Illinois 85486 </span>
+          </div>
+        </nav>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
