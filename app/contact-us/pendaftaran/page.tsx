e2b2e1871a2eb44660d98eb4ab2ac9a8899bb43f
Ramadhan <makbarramadhan62@gmail.com>
2024-06-10 09:16:01 +0700

    feat: add pendaftaran page

---
+++ app/contact-us/pendaftaran/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+export default function Pendaftaran() {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center overflow-x-hidden">
+      <Navbar />
+      <div className="flex-grow"></div>
+      <Footer />
+    </div>
+  );
+}
