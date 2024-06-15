30c96f08efa07bbacb0b1764aebfd59beebad10c
Ramadhan <makbarramadhan62@gmail.com>
2024-06-16 00:32:33 +0700

    refactor: adjust scroll link to link in a few elements

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -435,29 +435,29 @@ const Navbar = () => {
             </div>
 
             <div className="space-x-4">
-              <ScrollLink to="/">
+              <Link href="/">
                 <i className="bi bi-instagram text-blue-600 border border-blue-600 rounded-md p-2 hover:bg-blue-600 hover:text-white" />
-              </ScrollLink>
-              <ScrollLink to="/">
+              </Link>
+              <Link href="/">
                 <i className="bi bi-instagram text-blue-600 border border-blue-600 rounded-md p-2 hover:bg-blue-600 hover:text-white" />
-              </ScrollLink>
-              <ScrollLink to="/">
+              </Link>
+              <Link href="/">
                 <i className="bi bi-instagram text-blue-600 border border-blue-600 rounded-md p-2 hover:bg-blue-600 hover:text-white" />
-              </ScrollLink>
+              </Link>
             </div>
           </div>
         )}
 
         {/* Button Pendaftaran */}
         <div className="flex gap-6 font-semibold items-center">
-          <ScrollLink to="/contact-us">
+          <Link href="/contact-us">
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 focus:outline-none dark:focus:ring-blue-800"
             >
               Pendaftaran
             </button>
-          </ScrollLink>
+          </Link>
           <div className="hidden lg:flex space-x-4">
             <Link href="/">
               <i className="bi bi-instagram text-blue-600 border border-blue-600 rounded-md p-2 hover:bg-blue-600 hover:text-white" />
