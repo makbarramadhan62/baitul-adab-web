7f0ab67236475fb22a52cbb71410dd81bef0cfe6
Ramadhan <makbarramadhan62@gmail.com>
2024-06-16 16:47:12 +0700

    style: adjust responsive

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -161,7 +161,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="drawer">
+        <div className="drawer block lg:hidden">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content">
             {/* Page content here */}
